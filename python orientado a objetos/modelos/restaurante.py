496f65b6abcc781c24fe0118e0990d640a008190
iagord <rosadiasiago@gmail.com>
2024-02-15 14:10:59 -0300

    Continuação python orientado a objetos

--- python orientado a objetos/modelos/restaurante.py
+++ python orientado a objetos/modelos/restaurante.py
@@ -1,14 +1,32 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
     
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_pizza = Restaurante()
+    def __str__(self):
+        return f'{self._nome} | {self.categoria}'
+    
+    @classmethod
+    def listar_resataurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
+                  
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurante_praca.ativo)
+Restaurante.listar_resataurantes()
